a31e6168a111c25c730eb92ee31b62231a4beffb
Jubayer Ahmed <99324510+msJubayerahmed@users.noreply.github.com>
2022-04-15 23:28:06 +0400

    Add files via upload

---
+++ js/min.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
